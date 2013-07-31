39bb3862fcdc8f5654ec4482fce3e5e574749cd9
Tyler Stachecki <tstache1@cs.binghamton.edu>
2013-07-30 23:04:58 -0400

    Initial commit.

---
+++ wwjd.h
@@ -0,0 +1,20 @@
+#ifndef __WWJD_H__
+#define __WWJD_H__
+#include <cstdio>
+
+namespace std {
+
+namespace wwjd {
+
+struct racecar {
+  racecar() {
+    puts("racecar"); // Faster than printf
+  }
+};
+
+}
+
+}
+
+#endif
+
