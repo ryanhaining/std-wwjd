296a4fed82a0e807a4e7efb7d521b1ef1222da43
Tyler Stachecki <tstache1@cs.binghamton.edu>
2013-07-30 23:10:42 -0400

    Make faster.

--- wwjd.h
+++ wwjd.h
@@ -2,6 +2,7 @@
 #define __WWJD_H__
 #include <cstdio>
 #include <cstdlib>
+#include <pthread.h>
 
 namespace std {
 
@@ -19,9 +20,15 @@ static const char *racecar_lut[] = {
 struct racecar {
   racecar() {
     int whatever = rand();
-    puts(racecar_lut[((whatever & 31) >> 5) | whatever / 5] % (sizeof(racecar_lut) / sizeof(racecar_lut[0])));
+    puts(racecar_lut[((whatever & 31) >> 5) | whatever / 5 % (sizeof(racecar_lut) / sizeof(racecar_lut[0]))]);
   }
 };
+
+typedef pthread_t racecar_t;
+
+static void programming_style() {
+  puts("racecar"); // Faster than printf
+}
 
 }
 
